277cf63864adb070663f67461cccb28e8e1df065
dhruvbaraiya27 <baraiya.d@northeastern.edu>
2025-06-24 23:04:14 -0500

    Initial commit

---
+++ src/test/java/com/clouddeployment/clouddeployment/CloudDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clouddeployment.clouddeployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudDeploymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
